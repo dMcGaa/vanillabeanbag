0f97bd5b225beb3b4db2b698bd37ccb618c9c6c8
dmcgaa <mcgaa.david@gmail.com>
2016-02-14 17:12:48 +0000

    basic Angular page for favorite drinks

---
+++ public/js/app-drink.js
@@ -0,0 +1,24 @@
+(function() {
+  var app = angular.module('drinkStore', []);
+
+  app.controller('DrinkController', function() {
+    this.choices = drinks;
+  });
+
+  var drinks = [{
+    name: 'Mauby',
+    description: "Delicous Carribean Drink",
+    price: 2.50,
+    color: '#CCC',
+  }, {
+    name: 'Root Beer',
+    description: "Full body rooty flavor",
+    price: 2.50,
+    color: '#10C',
+  }, {
+    name: 'Dr Pepper',
+    description: "For all your medical drink needs.",
+    price: 2.50,
+    color: '#ABC',
+  }];
+})();
