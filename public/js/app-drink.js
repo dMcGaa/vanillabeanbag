517567ae073506ca7c5b05303e537479f1ee3b9a
dmcgaa <mcgaa.david@gmail.com>
2016-02-15 17:01:51 +0000

    Angular forms, Models, and Controllers

--- public/js/app-drink.js
+++ public/js/app-drink.js
@@ -13,22 +13,43 @@
       this.isSet = function(checkTab){
           return this.tab === checkTab;
       }
-  })
+  });
+  
+  app.controller('ReviewController', function(){
+    this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
   var drinks = [{
     name: 'Mauby',
     description: "Delicous Carribean Drink",
     price: 2.50,
     color: '#CCC',
+    reviews: [{
+        stars: 5,
+        body: "I love this drink!",
+        author: "awesomedude@example.org",
+        createdOn: 1397490000000
+      }, {
+        stars: 1,
+        body: "mauby sucks.",
+        author: "meany@example.org",
+        createdOn: 1397490000000
+      }]
   }, {
     name: 'Root Beer',
     description: "Full body rooty flavor",
     price: 2.50,
     color: '#10C',
+    reviews: []
   }, {
     name: 'Dr Pepper',
     description: "For all your medical drink needs.",
     price: 2.50,
     color: '#ABC',
+    reviews: []
   }];
 })();
