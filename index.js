72d5e023dc9b8b3778078d93bc27c13b601cf7f6
dmcgaa <mcgaa.david@gmail.com>
2016-02-24 16:07:30 +0000

    'GreenDesign ' site, first commit

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.get('/cuda', function(request, response) {
 app.get('/notify', function(request, response) {
   response.render('pages/site-notify')
 });
+app.get('/green-des', function(request, response) {
+  response.render('pages/site-green-design')
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
