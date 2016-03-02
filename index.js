503603624f7f790b99cb7fd5ada3ca280cd6a19d
dmcgaa <mcgaa.david@gmail.com>
2016-03-02 19:06:08 +0000

    'AH' additional page, new folder for new pages, some styling

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ app.get('/graff-pink', function(request, response) {
 app.get('/awesome-hosting', function(request, response) {
   response.render('pages/site-awesome-hosting')
 });
+app.get('/awesome-hosting/about-us', function(request, response) {
+  response.render('pages/awesome-hosting/about-us')
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
